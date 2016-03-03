a80eb54c6658cb82a6a7b294a872b6f5333c959f
Manuela <manujarare@hotmail.com>
2016-03-03 11:37:01 -0500

    prey .py 2

--- prey.py
+++ prey.py
@@ -7,26 +7,27 @@ import matplotlib.pyplot
 
 
 
-ky=2.0
-kyx=0.01
-kx=1.06
-kxy=0.01
-d=0.1
-x0=15
-y0=100
+Ky=2.0
+Kyx=0.01
+Kx=1.06
+Kxy=0.01
+D=0.1
+X0=15
+Y0=100
+x=range(101)
 t=range(101)
 y=range(101)
-y[0]=y0
-x[0]=x0
+y[0]=Y0
+x[0]=X0
 
 
-for i in range(1,101)
-	y1=(d*ky*y0)-(d*kxy*x0*y0)+y0
-	y0=y1
-	x1=(d*kyx*y0*x0)-(d*kx*x0)+x0
-	x0=x1
-        y[i]=y1
-	x[i]=x1
+for i in range(101):
+	Y1=(D*Ky*Y0)-(D*Kxy*X0*Y0)+Y0
+	Y0=Y1
+	X1=(D*Kyx*Y0*X0)-(D*Kx*X0)+X0
+	X0=X1
+        y[i]=Y1
+	x[i]=X1
 	t[i]=i*0.1
 	
 	
@@ -42,8 +43,8 @@ print ynew
 print xnew
 print tnew
 
-matplotlib.pyplot.plot(tnew,ynew)
-matplotlib.pyplot.plot(tnew,xnew)
+matplotlib.pyplot.plot(xnew,ynew)
+matplotlib.pyplot.plot(ynew,xnew)
 matplotlib.pyplot.show()
    
 
