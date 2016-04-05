5fcd41a8a647c558e1128b91c4c7254d4487c084
Manuela <manujarare@hotmail.com>
2016-04-05 11:42:49 -0500

    borracho

---
+++ pi.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib 
+import matplotlib.pyplot as plt
+
+
+#print np.random.random()
+#imprime numeros aleatorios en un rango de 0 
+#np.random.seed(6)
+#print np.random.random()
+
+
+N=100
+def fpi
+for i in range(N)
+	n= np.random.random()
+	y= np.random.random()
+	x= np.random.random()
+	pi=(L,R)**2*(n/N)
+	print pi
+	if np.sqrt(x,y
+
+	
