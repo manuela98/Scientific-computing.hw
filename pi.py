ee0c9518c101bba8acd3ff1f4dc937690a11ce37
Manuela <manujarare@hotmail.com>
2016-04-07 11:40:14 -0500

    pi

--- pi.py
+++ pi.py
@@ -8,15 +8,29 @@ import matplotlib.pyplot as plt
 #np.random.seed(6)
 #print np.random.random()
 
+R=1
+L=3
+N=10000
+n=0.0
 
-N=100
-def fpi
-for i in range(N)
-	n= np.random.random()
-	y= np.random.random()
-	x= np.random.random()
-	pi=(L,R)**2*(n/N)
-	print pi
-	if np.sqrt(x,y
+for i in range(N):
+
+	y= 1.5-np.random.random()*3
+	x= 1.5-np.random.random()*3
+	
+	puntoxy=np.sqrt(x**2+y**2)
+	
+	if puntoxy <=R:
+		n=n+1
+
+print n
+		
+def fpi(L,N,R):
+	print (n/float(N))*(L/R)**2
+
+print fpi(3.,10000,1.)
+
+
+
 
 	
